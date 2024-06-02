3d6333b0bea4181210dc56a0787de063381d3743
Nijin <nijin.lee@gmail.com>
2024-06-02 10:12:08 +0200

    combine two navbar streams

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,15 @@
 import { Routes } from '@angular/router';
-import { DashComponent } from './dash/dash.component';
-import { LoginComponent } from './login/login.component';
-import { AboutComponent } from './about/about.component';
-import { TeamComponent } from './team/team.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { DashComponent } from './components/dash/dash.component';
+import { LoginComponent } from './components/login/login.component';
+import { AboutComponent } from './components/about/about.component';
+import { TeamComponent } from './components/team/team.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
 import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
 import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
 
 export const routes: Routes = [
-    //{'path': '', 'title':'Dashboard', component: DashComponent},
+    {'path': 'dash', 'title':'Dashboard', component: DashComponent},
     {'path': 'about', 'title':'About', component: AboutComponent},
     {'path': 'team', 'title':'Team', component: TeamComponent},
     {'path': 'login', 'title':'Login', component: LoginComponent},
