c04e408122cb3a9874f64eab93b29ad29d406858
Nijin <nijin.lee@gmail.com>
2024-06-02 13:17:17 +0200

    refactoring website looks, added component food

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,12 +7,16 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
 import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
 import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
 import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
+import { FoodComponent } from './components/food/food.component';
 
 export const routes: Routes = [
+    //Cyberninja
     {'path': 'dash', 'title':'Dashboard', component: DashComponent},
     {'path': 'about', 'title':'About', component: AboutComponent},
     {'path': 'team', 'title':'Team', component: TeamComponent},
     {'path': 'login', 'title':'Login', component: LoginComponent},
+    {'path': 'food', component: FoodComponent },
+    //Bezkoder
     {'path': '', redirectTo: 'tutorials', pathMatch: 'full' },
     {'path': 'tutorials', component: TutorialsListComponent },
     {'path': 'tutorials/:id', component: TutorialDetailsComponent },
